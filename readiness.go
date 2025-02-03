bacbd2e6c61fc37c8bbe582c09bb6dbf5bd16371
marcus <marcus.chui.ca@icloud.com>
2025-02-03 05:13:30 -0500

    fix(readiness): remove HTTP method check for readiness handler

--- readiness.go
+++ readiness.go
@@ -6,10 +6,6 @@ import (
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write([]byte(http.StatusText(http.StatusOK))); err != nil {
