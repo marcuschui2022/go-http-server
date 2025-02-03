900c81d755e7b5d33f833ed478656ddf254ea536
marcus <marcus.chui.ca@icloud.com>
2025-02-03 05:13:55 -0500

    `fix: resolve unused parameter warning in readiness handler`

--- readiness.go
+++ readiness.go
@@ -6,6 +6,7 @@ import (
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	_ = r
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write([]byte(http.StatusText(http.StatusOK))); err != nil {
