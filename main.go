74e99ea52e8c39a5cb16b530f0ac01588f1adec0
marcus <marcus.chui.ca@icloud.com>
2025-02-03 04:40:15 -0500

    feat: add metrics tracking for file server hits
    - Introduced `apiConfig` with `fileserverHits` using `sync/atomic` for tracking.
    - Added middleware to increment file server hits.
    - Implemented `/metrics` endpoint to retrieve hit counts.
    - Implemented `/reset` endpoint to reset hit counts.
    - Enhanced `/healthz` handler to validate HTTP method.

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/metrics", handlerHitCount(&apiCfg))
+	mux.Handle("/reset", handlerHitCountReset(&apiCfg))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
@@ -21,7 +37,38 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+func handlerHitCount(apiCfg *apiConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		hits := apiCfg.fileserverHits.Load()
+		w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	}
+}
+
+func handlerHitCountReset(apiCfg *apiConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		apiCfg.fileserverHits.Store(0)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		hits := apiCfg.fileserverHits.Load()
+		w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	}
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
