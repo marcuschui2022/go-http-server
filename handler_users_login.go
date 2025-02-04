d05c7e7bb186324807c43afc7d95d2474b5487d4
marcus <marcus.chui.ca@icloud.com>
2025-02-04 12:51:38 -0500

    refactor(auth): improve error messages for user login
    - Update error messages to provide clearer context for login failures.
    - Remove unused fields from the JSON response in login handler.
    - Rename `password.go` and `password_test.go` to `auth.go` and `auth_test.go` respectively for better clarity and alignment.

--- handler_users_login.go
+++ handler_users_login.go
@@ -16,27 +16,25 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 	var p parameters
 	err := decoder.Decode(&p)
 	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "wrong id/password", err)
+		respondWithError(w, http.StatusUnauthorized, "Couldn't decode parameters", err)
 		return
 	}
 
 	user, err := cfg.db.GetUserByEmail(r.Context(), p.Email)
 	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "", err)
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
 		return
 	}
 
 	err = auth.CheckPasswordHash(p.Password, user.HashedPassword)
 	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "", err)
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
 		return
 	}
 
 	respondWithJSON(w, http.StatusOK, User{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
+		ID:    user.ID,
+		Email: user.Email,
 	})
 
 }
