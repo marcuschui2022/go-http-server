1ccd8275839088ca3289dfad1a80314f8ede0f9f
marcus <marcus.chui.ca@icloud.com>
2025-02-04 01:18:44 -0500

    refactor(api): replace `handlerChirpsValidate` with `handlerCreateChirp` for chirp creation and validation
    - Removed `handlerChirpsValidate` and related code.
    - Added `handlerCreateChirp` with chirp validation and database integration.
    - Updated `main.go` to remove validation handler and register new chirp creation route.
    - Simplified API prefix handling in `main.go`.

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"example.com/marcus/go-http-server/internal/database"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
-	"time"
 )
 
 type apiConfig struct {
@@ -50,7 +47,6 @@ func main() {
 	dbQueries := database.New(db)
 
 	const filepathRoot = "."
-	const apiPrefix = "/api/"
 	const port = "8080"
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
@@ -61,11 +57,10 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
 
-	mux.HandleFunc("GET "+apiPrefix+"healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
@@ -77,51 +72,4 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-type Chirp struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Body      string    `json:"body"`
-	UserID    uuid.UUID `json:"user_id"`
-}
-
-func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body   string    `json:"body"`
-		UserID uuid.UUID `json:"user_id"`
-	}
-	type resp struct {
-		Chirp
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
-		return
-	}
-
-	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
-		Body:   params.Body,
-		UserID: params.UserID,
-	})
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp", err)
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, resp{
-		Chirp: Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
-		},
-	})
-
 }
